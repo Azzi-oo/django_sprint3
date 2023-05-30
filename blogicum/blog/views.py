20ef953bb68d320dcfed37e419a50e03bc4a6784
Азат Янбердин <yanberdin.az@yandex.ru>
2023-05-30 14:26:42 +0500

    add change

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -8,21 +8,25 @@ datenow = timezone.now()
 
 def index(request):
     template = 'blog/index.html'
-    posts = Post.objects.select_related(
+    post_list = Post.objects.select_related(
         'category',
         'location',
         'author'
+    ).filter(
+        pub_date__lte=timezone.now(),
+        is_published=True,
+        category__is_published=True
     ).order_by(
         '-pub_date',
         'title',
     )[:5]
-    context = {'posts': posts}
+    context = {'post_list': post_list}
     return render(request, template, context)
 
 
 def post_detail(request, id):
     template = 'blog/detail.html'
-    posts = get_object_or_404(Post.objects.select_related(
+    post_list = get_object_or_404(Post.objects.select_related(
         'location', 'category', 'author').filter(
         pub_date__lte=timezone.now(),
         is_published=True,
@@ -30,7 +34,7 @@ def post_detail(request, id):
     ),
         pk=id
     )
-    context = {'post': posts}
+    context = {'post': post_list}
     return render(request, template, context)
 
 
@@ -40,7 +44,7 @@ def category_posts(request, category_slug):
         Category,
         slug=category_slug,
         is_published=True)
-    posts = get_list_or_404(
+    post_list = get_list_or_404(
         Post.objects.select_related(
             'category',
         ).filter(
@@ -50,5 +54,5 @@ def category_posts(request, category_slug):
         ),
         category__is_published=True
     )
-    context = {'category': category, 'posts': posts}
+    context = {'category': category, 'posts': post_list}
     return render(request, template, context)
